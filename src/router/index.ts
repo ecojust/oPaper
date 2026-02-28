cc83ca0e9b1da4e23b662753de824202e5a546cc
juzisang <995674251@qq.com>
2026-02-28 23:27:29 +0800

    fix(router): enable Stage and Background routes with static imports

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,18 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-// import Stage from "../views/Stage.vue";
-// import Background from "../views/Background.vue";
+import Stage from "../views/Stage.vue";
+import Background from "../views/Background.vue";
 
 const routes = [
   {
     path: "/",
     name: "Stage",
-    // component: Stage,
-    component: import("../views/Stage.vue"),
-
+    component: Stage,
     // component: BackgroundHTML,
   },
   {
     path: "/background",
     name: "Background",
-    // component: Background,
-    component: import("../views/Background.vue"),
+    component: Background,
   },
 ];
 
